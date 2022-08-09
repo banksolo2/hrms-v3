a8978c79dac23640676ed5d908ab5c6cdb41f973
banksolo2 <seunolo2@gmail.com>
2022-08-09 17:06:24 +0100

    First commit
    First commit

---
+++ src/main/java/com/bankstech/hrmsv3/HrmsV3Application.java
@@ -0,0 +1,13 @@
+package com.bankstech.hrmsv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrmsV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrmsV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/bankstech/hrmsv3/HrmsV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankstech.hrmsv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
